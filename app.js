e76574ab85fec8fcd9ba33353d49790dbe0272a1
Abdelmoumene Mameri <_amameri.az@sodexo.com>
2025-10-08 15:06:42 +0200

    app.js commit

---
+++ app.js
@@ -0,0 +1,16 @@
+// app.js (vulnerable example)
+const express = require('express');
+const app = express();
+// assume `db.query(sql, cb)` runs SQL against a DB
+
+app.get('/search', (req, res) => {
+  const q = req.query.q;
+  // BAD: directly interpolating user input into SQL-like string
+  const sql = `SELECT * FROM products WHERE name = '${q}'`;
+  db.query(sql, (err, rows) => {
+    if (err) return res.status(500).send('error');
+    res.send(rows);
+  });
+});
+
+app.listen(3000, () => console.log('listening on 3000'));
